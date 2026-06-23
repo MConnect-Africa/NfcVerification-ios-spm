a75c02f7ba452c7aecf0e977fb8005dd73552514
Samuel Muigai <samuel.muigai@mouvafrica.com>
2026-06-23 22:35:20 +0300

    minor code clean up

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
 // swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -9,26 +8,15 @@ let package = Package(
         .iOS(.v15),
     ],
     products: [
-        // The public library now points to the wrapper target
         .library(
             name: "NfcSdk",
-            targets: ["NfcSdk"]
+            targets: ["NfcSdkBinary"]  // ← point product directly at the binary
         ),
     ],
     dependencies: [
         .package(url: "https://github.com/AndyQ/NFCPassportReader.git", exact: "2.3.0")
     ],
     targets: [
-
-        .target(
-            name: "NfcSdk",
-            dependencies: [
-                .target(name: "NfcSdkBinary"),
-                .product(name: "NFCPassportReader", package: "NFCPassportReader")
-            ],
-            path: "Sources/NfcVerification"
-        ),
-        
         .binaryTarget(
             name: "NfcSdkBinary",
             url: "https://github.com/MConnect-Africa/NfcVerification-ios-spm/releases/download/v1.0.0/NfcSdk.xcframework.zip",
--- Sources/NfcVerification/dummy.swift
+++
@@ -1,7 +0,0 @@
-//
-//  dummy.swift
-//  NfcVerification-spm
-//
-//  Created by mam on 23/06/2026.
-//
-
